8690eed5ae4043144139bb97388a8228bc4d0ebc
Arghya Jana <arghya0017@gmail.com>
2024-09-01 00:23:51 +0530

    added basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import Signin from "./pages/signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">SIH 2024</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/sign-in" element={<Signin />} />
+            <Route path="/sign-up" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
