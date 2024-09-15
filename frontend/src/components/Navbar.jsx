7f04ec60b3844f499737b7d224d968682d4f84bd
Arghya Jana <arghya0017@gmail.com>
2024-09-16 04:23:51 +0530

    Updated Navbar, package files, and added avatar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import { ModeToggle } from "./ModeToggle";
 import {
@@ -13,8 +13,19 @@ import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Button } from "@headlessui/react";
+import { useSelector } from "react-redux";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="fixed z-10 w-full shadow-md bg-clip-padding">
       <div
@@ -45,37 +56,76 @@ const Navbar = () => {
           </Button>
         </div>
         <div className="hidden gap-5 items-center font-medium lg:flex">
-          <Link
+          <NavLink
             to="/"
-            className="hover:text-violet-400 active:text-violet-500  focus:text-violet-600"
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 font-bold" : "text-gray-500"
+            }
           >
             Home
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/about"
-            className="hover:text-violet-400 active:text-violet-500  focus:text-violet-600"
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 font-bold" : "text-gray-500"
+            }
           >
             About
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/Features"
-            className="hover:text-violet-400 active:text-violet-500 focus:text-violet-600"
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 font-bold" : "text-gray-500"
+            }
           >
             Features
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/emergency"
-            className="text-white bg-red-500 rounded-md border-2 border-red-600 hover:text-violet-300 active:boder-balck focus:border-black dark:focus:border-white px-1
-            "
+            className={({ isActive }) =>
+              isActive
+                ? "text-white bg-red-500 rounded-md border-2 border-red-600 px-2"
+                : "text-white bg-red-400 rounded-md border-2 border-red-500 hover:font-bold px-2 "
+            }
           >
             Emergency
-          </Link>
+          </NavLink>
         </div>
         <div className="flex gap-4">
           <ModeToggle />
-          <Button className="border bg-black text-white dark:bg-white dark:text-black px-3 rounded-md">
-            SignIn
-          </Button>
+          {currentUser ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src={currentUser.profilePicture} />
+                  <AvatarFallback>User</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  <span className="">@{currentUser.username}</span>
+                </DropdownMenuLabel>
+
+                <DropdownMenuItem>
+                  <span className="trunate">{currentUser.email}</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem>
+                  <Link to={"/dashboard?tab=profile"}>Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem>Sign out</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <Link to="/sign-in">
+              <Button className="border bg-black text-white dark:bg-white dark:text-black px-3 rounded-md">
+                SignIn
+              </Button>
+            </Link>
+          )}
         </div>
 
         <div className="flex md:hidden">
@@ -85,11 +135,25 @@ const Navbar = () => {
             </SheetTrigger>
             <SheetContent className="flex flex-col gap-4 w-[180px] md:w-[300px]">
               <SheetClose asChild>
-                <Link to="/">Home</Link>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "text-blue-500 font-bold" : "text-gray-500"
+                  }
+                >
+                  Home
+                </NavLink>
               </SheetClose>
               <Separator />
               <SheetClose asChild>
-                <Link to="/about">About</Link>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    isActive ? "text-blue-500 font-bold" : "text-gray-500"
+                  }
+                >
+                  About
+                </NavLink>
               </SheetClose>
               <Separator />
               <SheetClose asChild>
