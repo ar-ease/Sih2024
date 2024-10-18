439c919a3b3728fe3d6053458fa3ded6ff59edad
Arghya Jana <arghya0017@gmail.com>
2024-10-18 13:09:13 +0530

    delete message server

--- backend/api/controllers/comment.controller.js
+++ backend/api/controllers/comment.controller.js
@@ -85,3 +85,23 @@ export const editComment = async (req, res, next) => {
   try {
   } catch (error) {}
 };
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user.id) {
+      return next(
+        errorHandler(403, "You are not authorized to delete this comment")
+      );
+    }
+    await Comment.findByIdAndDelete(req.params.commentId);
+
+    res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (error) {
+    return next(errorHandler(500, "Something went wrong"));
+  }
+};
